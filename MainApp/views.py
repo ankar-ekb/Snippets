884b54c1262a2d694e28c9426f7aadb42b1b9c2f
Anton <antony.u.karev@yandex.ru>
2024-03-28 20:53:52 +0300

    Django form for snippet add

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseNotFound
+from MainApp.Forms import SnippetForm
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -10,8 +11,20 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET": # получаем чистую форму для заполнения
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+            }
+        return render(request, 'pages/add_snippet.html', context)
+    
+    if request.method == "POST": # создаем новый сниппет с данными из формы
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('snippets-list')
+
 
 
 def snippets_page(request):
@@ -20,6 +33,7 @@ def snippets_page(request):
         'pagename': 'Просмотр сниппетов',
         'snippets': snippets
         }
+    
     return render(request, 'pages/view_snippets.html', context)
 
 def snippet_detail(request, id):
@@ -34,3 +48,12 @@ def snippet_detail(request, id):
         "snippet": snippet
     }
     return render(request, "pages/snippet_detail.html", context)
+
+# def snippet_create(request):
+#     if request.method == 'POST':
+#         name = request.POST['name']
+#         lang = request.POST['lang']
+#         code = request.POST['code']
+#         snippet = Snippet(name=name, lang=lang, code=code)
+#         snippet.save()
+#         return redirect('snippets-list')
